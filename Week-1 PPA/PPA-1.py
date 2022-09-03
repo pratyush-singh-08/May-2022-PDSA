0644819bf3dd7965bdc109876eedcdff963feba4
Pratyush Singh <pratyush.singh@meritech.co.in>
2022-09-03 16:26:09 +0530

    commit

--- Week-1 PPA/PPA-1.py
+++ Week-1 PPA/PPA-1.py
@@ -1,1 +1,26 @@
-print("hi");
+def Prime(x):
+  c=0
+  for i in range(1,x+1):
+    if x%i==0:
+      c+=1
+  if c==2:
+    return(True)
+  else:
+    return(False)
+
+def prime_product(m):
+  l=[]
+  for i in range(1,m+1):
+    if Prime(i):
+      l.append(i)
+  
+  for i in l:
+    for j in l:
+      if i*j==m:
+        return(True)
+  return(False)
+
+
+
+n = int(input())
+print(prime_product(n))
